c3e05f995f40f59c46ad7505a9fe411e3dae624c
Deepak Venkatesh <50081353+PeepalCapital@users.noreply.github.com>
2025-04-06 07:14:08 -0700

    Create EvenGenerator.go

---
+++ Chapter 6/EvenGenerator.go
@@ -0,0 +1,24 @@
+/* Closure example. Common Lisp closures seem very elegant to me. This makes me a little bit uncomfortable */
+
+package main
+
+import "fmt"
+
+func makeEvenGenerator() func() uint {
+	i := uint(0)
+	return func() (ret uint) {
+		ret = i
+		i += 2
+		return
+	}
+}
+
+func main() {
+	nextEven := makeEvenGenerator()
+	fmt.Println(nextEven())
+	fmt.Println(nextEven())
+	fmt.Println(nextEven())
+	fmt.Println(nextEven())
+	fmt.Println(nextEven())
+	fmt.Println(nextEven())
+}
